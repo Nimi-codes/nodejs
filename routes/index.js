aabea425815af511348c7e56328f11e53611eb30
Nimisha Mishra <nimishamishrastp@gmail.com>
2020-10-04 01:24:17 +0530

    New router and controller created

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 console.log('Router called');
 router.get('/',homeController.home)
+router.use('/users',require('./users'))
+//for any further routes access from here
+//router,use
 module.exports=router;
